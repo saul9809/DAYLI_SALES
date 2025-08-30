8fd5a1f86d654e5e2268cf05e82b5a45d9ec5d07
Saul9809 <120270819+saul9809@users.noreply.github.com>
2025-08-30 00:20:58 -0400

    trying to deploy again

--- tests/Pest.php
+++ tests/Pest.php
@@ -8,11 +8,12 @@
 | The closure you provide to your test functions is always bound to a specific PHPUnit test
 | case class. By default, that class is "PHPUnit\Framework\TestCase". Of course, you may
 | need to change it using the "pest()" function to bind a different classes or traits.
-|
-*/
+/*"build": "vite build",
+        //"build:ssr": "vite build && vite build --ssr",*/
+
 
 pest()->extend(Tests\TestCase::class)
- // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
+    // ->use(Illuminate\Foundation\Testing\RefreshDatabase::class)
     ->in('Feature');
 
 /*
